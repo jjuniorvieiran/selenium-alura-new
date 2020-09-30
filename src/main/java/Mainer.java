92d1cba86e9a3c4c37553ad7d430a4c59d3721f3
Junior <junior.jaava@gmail.com>
2020-09-30 19:23:20 +0200

    first commit

---
+++ src/main/java/Mainer.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Mainer {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com.br/");
+
+        WebElement campoDeTexto = driver.findElement(By.name("q"));
+        campoDeTexto.sendKeys("Caelum");
+
+        campoDeTexto.submit();
+    }
+}
