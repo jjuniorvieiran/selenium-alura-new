c19f16835acdca00f8b732416ab9079e251f53cd
Junior <junior.jaava@gmail.com>
2020-10-02 16:06:39 +0200

    - class where was learned txt field, select field, radio fieldand test scenarios

---
+++ src/test/java/LeiloesPage.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+class LeiloesPage {
+
+    private WebDriver driver;
+
+    public LeiloesPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void visita() {
+        driver.get("http://localhost:8080/leiloes");
+    }
+
+    public NovoLeilaoPage novo() {
+        // clica no link de novo leilao
+        driver.findElement(By.linkText("Novo Leilão")).click();
+        // retorna a classe que representa a nova pagina
+        return new NovoLeilaoPage(driver);
+    }
+
+    public boolean existe(String produto, double valor, String usuario,
+                          boolean usado) {
+
+        return driver.getPageSource().contains(produto) &&
+                driver.getPageSource().contains(String.valueOf(valor)) &&
+                driver.getPageSource().contains(usado ? "Sim" : "Não");
+
+    }
+}
